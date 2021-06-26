bf514e24aa1b51bca646ab3bf71ebcfd519a93fb
Luis Antonio <luis.antonio1101@gmail.com>
2021-06-26 19:36:06 -0300

    Adicionar arquivos de projeto.

---
+++ chainOfResponsabilityExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using chainOfResponsabilityExample.Models;
+using System;
+
+namespace chainOfResponsabilityExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDesconto d1 = new DescontoPorCincoItens();
+            IDesconto d2 = new DescontoPorMaisDeQuinhentosReais();
+            IDesconto d3 = new DescontoPorVendaCasada();
+            IDesconto d4 = new SemDesconto();
+
+            d1.Proximo = d2;
+            d2.Proximo = d3;
+            d3.Proximo = d4;
+
+            Orcamento orcamento = new Orcamento(500.0);
+
+            double desconto = d1.Desconta(orcamento);
+            Console.WriteLine("Desconto total: " + desconto);
+        }
+    }
+}
