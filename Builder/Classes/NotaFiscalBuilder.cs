c35d3387e4ec2c39e6ad591f530a64f09ad45835
Luis Antonio <luis.antonio1101@gmail.com>
2021-07-04 16:30:44 -0300

    Builder

---
+++ Builder/Classes/NotaFiscalBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Classes
+{
+    public class NotaFiscalBuilder
+    {
+        public String RazaoSocial { get; set; }
+        public String CNPJ { get; set; }
+        public String Observacoes { get; set; }
+        public DateTime DataAtual { get; set; }
+
+
+        private double ValorTotal { get; set; }
+        private double Impostos { get; set; }
+        private IList<ItemDaNota> Itens = new List<ItemDaNota>(); 
+
+
+        public NotaFiscal builder()
+        {
+            return new NotaFiscal(RazaoSocial, CNPJ, DataAtual, ValorTotal, Impostos, Itens, Observacoes);
+        }
+        public NotaFiscalBuilder ParaEmpresa(String razaoSocial)
+        {
+            this.RazaoSocial = razaoSocial;
+            return this;
+        }
+        public NotaFiscalBuilder ComCNPJ(String cnpj)
+        {
+            this.CNPJ = cnpj;
+            return this;
+        }
+
+        public NotaFiscalBuilder ComObservacoes(String obs)
+        {
+            this.Observacoes = obs;
+            return this;
+        }
+        public NotaFiscalBuilder NaDataAtual()
+        {
+            this.DataAtual = DateTime.Now;
+            return this;
+        }
+
+
+
+        public NotaFiscalBuilder ComItem(ItemDaNota item)
+        {
+            this.Itens.Add(item);
+            ValorTotal += item.Valor;
+            Impostos += item.Valor * 0.02;
+            return this;
+        }
+
+
+
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Builder.Classes;
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotaFiscalBuilder builder = new NotaFiscalBuilder();
+            builder.ParaEmpresa("empresa nome")
+                   .ComCNPJ("1233132312")
+                   .ComItem(new ItemDaNota("bola", 50.50))
+                   .ComObservacoes("obs1")
+                   .NaDataAtual();
+            NotaFiscal nf = builder.builder();
+
+        }
+    }
+}
